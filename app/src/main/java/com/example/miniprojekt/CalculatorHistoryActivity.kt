eb18139420346cfeb01f48bf1ca38de54bf70cac
djroumen <64631221+djroumen@users.noreply.github.com>
2020-10-22 08:23:42 +0200

    add EmptyList pic to History

--- app/src/main/java/com/example/miniprojekt/CalculatorHistoryActivity.kt
+++ app/src/main/java/com/example/miniprojekt/CalculatorHistoryActivity.kt
@@ -3,13 +3,16 @@ package com.example.miniprojekt
 import android.content.Context
 import android.os.Build
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import controller.CalculatorHistoryController
 import data.HistoryEntry
+import kotlinx.android.synthetic.main.activity_calculatorhistory.view.*
 import java.io.File
 
 class CalculatorHistoryActivity : AppCompatActivity(){
@@ -22,6 +25,7 @@ class CalculatorHistoryActivity : AppCompatActivity(){
         calculatorHistoryController =  CalculatorHistoryController(this.baseContext)
 
         val recyclerView : RecyclerView = findViewById<RecyclerView>(R.id.historyList)
+        val imageEmptyList : ImageView = findViewById<ImageView>(R.id.imageEmptyList)
 
         val layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
         recyclerView.layoutManager = layoutManager
@@ -32,11 +36,25 @@ class CalculatorHistoryActivity : AppCompatActivity(){
         val historyAdapter : HistoryAdapter = HistoryAdapter(history)
         recyclerView.adapter = historyAdapter
 
+        val changeVisibilityFn = fun () {
+            if (history.isEmpty()) {
+                recyclerView.visibility = View.GONE;
+                imageEmptyList.visibility = View.VISIBLE;
+            }
+            else {
+                recyclerView.visibility = View.VISIBLE;
+                imageEmptyList.visibility = View.GONE;
+            }
+        }
+        changeVisibilityFn()
         val btnReset: Button = findViewById<Button>(R.id.btn_clearHistory);
         btnReset.setOnClickListener {
             calculatorHistoryController.clearList()
             historyAdapter.notifyDataSetChanged()
+            changeVisibilityFn();
         }
+
+
     }
 }
 
